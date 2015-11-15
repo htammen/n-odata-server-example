8620fd978e373400465f0087c9e2625e24a014a7
Johannes Hamel <johannes.hamel@gmail.com>
2015-11-15 13:41:16 +0100

    Integration Test json.

--- test/odata.integration.test.js
+++ test/odata.integration.test.js
@@ -2,12 +2,15 @@ var lt = require('loopback-testing');
 var expect = require("chai").expect;
 var app = require('../server/server.js');
 
-describe('/odata/', function() {
+describe('/odata/', function () {
 	lt.beforeEach.withApp(app);
-	lt.describe.whenCalledRemotely('GET', '/odata/', function() {
+	lt.describe.whenCalledRemotely('GET', '/odata/', function () {
 		lt.it.shouldBeAllowed();
-		it('should have statusCode 200', function() {
+		it('should have statusCode 200', function () {
 			expect(this.res.statusCode).to.equal(200);
-		});
+		})
+		it('should be json', function () {
+			expect(this.res).to.be.json;
+		})
 	});
 });
