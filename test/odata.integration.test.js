b095332448af8ede9aee1a0082bfcaeebc9a8d75
Johannes Hamel <johannes.hamel@gmail.com>
2015-11-15 13:15:30 +0100

    Clean up integration test.

--- test/odata.integration.test.js
+++ test/odata.integration.test.js
@@ -1,7 +1,6 @@
 var lt = require('loopback-testing');
 var expect = require("chai").expect;
-
-var app = require('../server/server.js'); //path to app.js or server.js
+var app = require('../server/server.js');
 
 describe('/odata', function() {
 	lt.beforeEach.withApp(app);
@@ -9,11 +8,6 @@ describe('/odata', function() {
 		lt.it.shouldBeAllowed();
 		it('should have statusCode 200', function() {
 			expect(this.res.statusCode).to.equal(200);
-			expect(this.res.body).to.equal(200);
 		});
-		it('should respond with an array of models', function() {
-			expect(Array.isArray(this.res.body)).to.be.true;
-		});
-
 	});
 });
