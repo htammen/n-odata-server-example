bbaed9433e4f947373d7e68b465a6a500810e87a
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2016-01-08 08:30:33 +0100

    Added Category and a hasMany relationship to Product. Sample data for this is generated via the generation of products

--- server/sampledata/products.js
+++ server/sampledata/products.js
@@ -1,71 +1,114 @@
 var fs = require('fs');
 var createObjects = require('./createObjects');
 
-module.exports = new Promise(function(resolve, reject) {
+module.exports = new Promise(function (resolve, reject) {
 
 })
 
 /**
  * Creates products and a productDetail for each product
  */
-module.exports = function(app) {
-	return new Promise(function(resolve, reject) {
-		app.dataSources.db.automigrate(['Product', 'ProductDetail'], function(err) {
+module.exports = function (app) {
+	return new Promise(function (resolve, reject) {
+		app.dataSources.db.automigrate(['Category', 'Product', 'ProductDetail'], function (err) {
 			if (err) throw err;
 			var Product = app.models.Product;
+			var Category = app.models.Category;
+
+			var categoryContent = fs.readFileSync('storage/sampledata/categories.json', 'utf8');
+			var categories = JSON.parse(categoryContent);
 
 			var fileContent = fs.readFileSync('storage/sampledata/products.json', 'utf8');
 			var objects = JSON.parse(fileContent);
 
 			var obj;
-			var arrPromises = [];
-			var arrErrors = [];
-			for(var i=0; i<objects.length; i++) {
-				obj = objects[i];
-				arrPromises.push(Product.create(obj).then(
-					function(object) {
-						// product created successfully. Create a ProductDetail now
-						var detailText = object.Name + '_Detail';
-						object['details'].create({
-							'Details': detailText
-						}).then(
-							function(objDetail) {
-								console.log('Details: ', objDetail);
-								console.log(Product.definition.name, object);
-							},
-							function(err) {
-								arrErrors.push(err);
-								console.error(err);
-							}
-						);
-					},
-					function(err) {
-						arrErrors.push(err);
-					})
-				)
-			}
+			var arrCategories = [];
 
-			Promise.all(arrPromises).then(function() {
-				if (arrErrors.length > 0) {
-					console.error("Promise.all succeeded with " + arrErrors.length + " errors");
-					reject(arrErrors);
-				}
-				else {
-					resolve();
-				}
-			}, function(err) {
-				console.error("Promise.all failed");
-				reject(err);
+			var promiseCategory = new Promise(function (resolve, reject) {
+				categories.forEach(function (category) {
+					Category.create(category).then(function (newCategory) {
+						console.log('Category: ' + newCategory);
+						arrCategories.push(newCategory);
+						if (arrCategories.length === categories.length) {
+							resolve();
+						}
+					})
+				});
 			});
+
+
+			promiseCategory.then(function () {
+				new Promise(function (resolve, reject) {
+					var productsCount = 0;
+					var arrErrors = [];
+
+					var checkFuncEnd = function() {
+						productsCount++;
+						if (productsCount === objects.length) {
+							resolve(arrErrors);
+						}
+					};
+
+					for (var i = 0; i < objects.length; i++) {
+						obj = objects[i];
+						Product.create(obj).then(
+							function (object) {
+								// product created successfully. Create a ProductDetail now
+								var detailText = object.Name + '_Detail';
+								object['ProductDetails'].create({
+									'Details': detailText
+								}).then(
+									function (objDetail) {
+										console.log('ProductDetails: ', objDetail);
+										console.log(Product.definition.name, object);
+										checkFuncEnd();
+									},
+									function (err) {
+										arrErrors.push(err);
+										console.error(err);
+										checkFuncEnd();
+									}
+								);
+							},
+							function (err) {
+								arrErrors.push(err);
+								checkFuncEnd();
+							}
+						)
+					}
+				}).then(function (arrErrors) {
+					if (arrErrors.length > 0) {
+						console.error("Promise.all succeeded with " + arrErrors.length + " errors");
+						reject(arrErrors);
+					}
+					else {
+						resolve();
+					}
+				})
+			});
+
+
+			//Promise.all(arrPromises).then(function() {
+			//	if (arrErrors.length > 0) {
+			//		console.error("Promise.all succeeded with " + arrErrors.length + " errors");
+			//		reject(arrErrors);
+			//	}
+			//	else {
+			//		resolve();
+			//	}
+			//}, function(err) {
+			//	console.error("Promise.all failed");
+			//	reject(err);
+			//});
 
 		});
 
 
 		/*		createObjects(objects, Product).then(function() {
-					cb(null);
-				}, function(errCount) {
-					cb(errCount);
-				});
-		*/
+		 cb(null);
+		 }, function(errCount) {
+		 cb(errCount);
+		 });
+		 */
 	});
 };
